046d9dca2ffaefbf2d44f41f59b2bab29f794937
JuliaIzotova] <yuizotova15@gmail.com>
2022-03-28 15:54:02 +0300

    first commit

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -9,9 +9,8 @@ class SQRServiceTest {
 
     @ParameterizedTest
     @CsvSource(value = {"'Limits', 200, 300, 3",
-            "'Critical borders', 100, 9801, 90",
-            "'Over 9801', 9802, 100000, 0",
-            "'Over 99', 99, 1, 0"})
+            "'Under limits', 10, 90, 0",
+            "'Over limits', 100, 9801, 90"})
     void shouldCalculateSqr(String testName, int underLimit, int overLimit, int expected) {
         SQRService service = new SQRService();
         int actual = service.calculateSqr(underLimit, overLimit);
